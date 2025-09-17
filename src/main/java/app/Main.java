e34a7b12bc5064f3a597b91330d15618a9a58b37
Asger <asgersh@hotmail.com>
2025-09-17 10:55:50 +0200

    Implementeret metode for top10 populære film

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,15 +1,27 @@
 package app;
 
 import app.config.HibernateConfig;
+import app.daos.MovieDAO;
+import app.entities.Movie;
 import app.services.MovieService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
         MovieService movieService = new MovieService();
+        MovieDAO movieDAO = new MovieDAO(emf);
+
+        // Top 10 mest populære film
+        List<Movie> top10Popular = movieDAO.topTenPopularMovies();
+        top10Popular.forEach(System.out::println);
+
+
 
         // Dette er udkommenteret - så vi ikke skal vente på alle 5000 film hver gang
 
